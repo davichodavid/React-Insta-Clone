16ec21e2d7a2d6c4a5cfc5adb077356ad220b3d2
davichodavid <davichodavid@yahoo.com>
2019-04-30 22:26:13 -0400

    got the comment section to work.. god bless

--- instagram-app/src/components/CommentSection/CommentSection.js
+++ instagram-app/src/components/CommentSection/CommentSection.js
@@ -1,26 +1,25 @@
 import React from 'react';
 import '../PostContainer/PostContainer.css';
 
-class CommentSection extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      comments: props.comments,
-      comment: ''
-    }
-  }
+const CommentSection = (props) => {
+  // constructor(props) {
+  //   super(props);
+  //   this.state = {
+  //     comments: props.comments,
+  //     comment: '',
+  //   }
+  // }
 
-  render() {
-    return (
-      <form onSubmit="" className="comment-form">
-        <input
-          type="text"
-          placeholder="Add a comment..."
-          value=""
-        />
-      </form>
-    );
-  }
+  return (
+    <form onSubmit={props.addNewComment} className="comment-form">
+      <input
+        type="text"
+        placeholder="Add a comment..."
+        value={props.comment}
+        onChange={props.handleChanges}
+      />
+    </form>
+  );
 }
 
 export default CommentSection;
--- instagram-app/src/components/PostContainer/PostContainer.js
+++ instagram-app/src/components/PostContainer/PostContainer.js
@@ -3,29 +3,58 @@ import './PostContainer.css';
 
 import CommentSection from '../CommentSection/CommentSection'
 
-const PostContainer = (props) => {
+class PostContainer extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      comments: props.dummyData.comments,
+      comment: ''
+    }
+  }
 
-  return (
-    <div className="post">
-      <div className="post-header">
-        <img src={props.dummyData.thumbnailUrl} alt="thumbnail" />
-        <h1>{props.dummyData.username}</h1>
+  addNewComment = event => {
+    console.log(this.state.comments)
+    event.preventDefault();
+    this.setState({
+      comments: [...this.state.comments,
+      {
+        username: 'davichodavid',
+        text: this.state.comment
+      }
+      ],
+      comment: ''
+    })
+  }
+
+  handleChanges = event => {
+    this.setState({
+      comment: event.target.value
+    });
+  };
+
+  render() {
+    return (
+      <div className="post">
+        <div className="post-header">
+          <img src={this.props.dummyData.thumbnailUrl} alt="thumbnail" />
+          <h1>{this.props.dummyData.username}</h1>
+        </div>
+        <img className="post-img" src={this.props.dummyData.imageUrl} alt="post" />
+        <div className="like-comment">
+          <button><i className="far fa-heart"></i></button>
+          <button><i className="far fa-comment"></i></button>
+        </div>
+        <h1 className="likes">Likes: {this.props.dummyData.likes}</h1>
+        <div className="this-wont-work">
+          {this.state.comments.map((comment, index) => {
+            return <h1 key={index}>{comment.username}<p>{comment.text}</p></h1>;
+          })}
+        </div>
+        <p className="time-stamp">{this.props.dummyData.timestamp}</p>
+
+        <CommentSection comments={this.props.dummyData.comments} addNewComment={this.addNewComment} handleChanges={this.handleChanges} comment={this.state.comment} />
       </div>
-      <img className="post-img" src={props.dummyData.imageUrl} alt="post" />
-      <div className="like-comment">
-        <button><i className="far fa-heart"></i></button>
-        <button><i className="far fa-comment"></i></button>
-      </div>
-      <h1 className="likes">Likes: {props.dummyData.likes}</h1>
-      <div className="this-wont-work">
-        {props.dummyData.comments.map((comment, index) => {
-          return <h1 key={index}>{comment.username}<p>{comment.text}</p></h1>;
-        })}
-      </div>
-      <p className="time-stamp">{props.dummyData.timestamp}</p>
-      <CommentSection />
-    </div>
-  )
+    )
+  }
 }
-
 export default PostContainer;
