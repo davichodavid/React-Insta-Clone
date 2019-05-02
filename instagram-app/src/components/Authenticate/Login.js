cd0983fdd5d291d821d2015814dd9a0dd9cf1783
davichodavid <davichodavid@yahoo.com>
2019-05-02 16:02:20 -0400

    Starting to work on the login functionality and reading about local storage

---
+++ instagram-app/src/components/Authenticate/Login.js
@@ -0,0 +1,26 @@
+import React from 'react';
+
+class Login extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      password: '',
+      username: ''
+    };
+  }
+
+  render() {
+    <div>
+      <form>
+        <input />
+        <input />
+      </form>
+    </div>
+  }
+
+
+
+
+}
+
+export default Login;
